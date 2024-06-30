f99adac0cbb4502c72e76a99c25893ae09438d8f
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2024-06-30 19:43:46 -0300

    Created repositories

---
+++ src/main/java/com/one/literalura/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.one.literalura.repository;
+
+import com.one.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
